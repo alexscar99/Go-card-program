b06af5a6aefc5ab897eea4e846b8e4aa765d6161
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-19 03:31:57 -0400

    created newDeckFromFile func to read file and return a deck from it

--- deck.go
+++ deck.go
@@ -3,31 +3,30 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
 type deck []string
 
 func newDeck() deck {
-	// initialize empty var of type deck
 	cards := deck{}
 
-	// two string slices, one for suits and one for values
+	// two string slices, one for suits another for values
 	cardSuits := []string{"Spades", "Clubs", "Diamonds", "Hearts"}
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
-	// iteration to append each card, `_` is used as placeholder for the index vars not needed
+	// iteration to append each card to deck
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
 
-	// return var of type deck with all cards
+	// return deck with all cards
 	return cards
 }
 
-// receiver of type deck --> any var of type deck has access to print func
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
@@ -39,12 +38,23 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-// helper func to turn deck to string
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-// save deck as a file
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// val of type err will have value of `nil` if nothing went wrong
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
